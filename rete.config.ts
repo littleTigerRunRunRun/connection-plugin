b4b831f679bb3bf29c618dafc02a674a3451f589
小老虎冲冲冲 <36033943+littleTigerRunRunRun@users.noreply.github.com>
2026-04-07 17:37:50 +0800

    feat(check): 生成连线检测

--- rete.config.ts
+++ rete.config.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { ReteOptions } from 'rete-cli'
-import sass from 'rollup-plugin-sass'
+// import sass from 'rollup-plugin-sass'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
   name: 'ReteConnectionPlugin',
-  plugins: [
-    sass({
-      insert: true
-    })
-  ],
+  // plugins: [
+    // sass({
+    //   insert: true
+    // })
+  // ],
   globals: {
     'rete': 'Rete',
     'rete-area-plugin': 'ReteAreaPlugin'
