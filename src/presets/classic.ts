53695d1a7b2ba29437d61fff97d30280de7b736d
小老虎冲冲冲 <36033943+littleTigerRunRunRun@users.noreply.github.com>
2026-04-09 17:51:47 +0800

    feat(params): 提供了connection配置入口

--- src/presets/classic.ts
+++ src/presets/classic.ts
@@ -1,8 +1,9 @@
-import { ClassicFlow } from '../flow'
+import { ClassicFlow, ClassicParams } from '../flow';
+import { ClassicScheme } from '../types';
 
 /**
  * Classic preset. Uses `ClassicFlow` for managing connections by user
  */
-export function setup() {
-  return () => new ClassicFlow()
+export function setup<Schemes extends ClassicScheme>(params?: Partial<ClassicParams<Schemes>>) {
+  return () => new ClassicFlow<Schemes, any[]>(params)
 }
